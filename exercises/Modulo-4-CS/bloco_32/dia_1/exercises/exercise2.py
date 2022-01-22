a5d98e4b7eece905af082e135962f9e7d54f0e6b
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2022-01-22 16:04:49 -0300

    adiciona exercicio 2

---
+++ exercises/Modulo-4-CS/bloco_32/dia_1/exercises/exercise2.py
@@ -0,0 +1,5 @@
+def arithmetic_mean(numbers_list):
+    return f"A média aritmética é = {sum(numbers_list) / len(numbers_list)}"
+
+
+print(arithmetic_mean([2, 3, 3, 5, 7, 10]))  # A média aritmética é = 5.0
