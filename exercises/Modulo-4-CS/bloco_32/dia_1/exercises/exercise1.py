a4ebdb6daa2bf690ddc9f9e6fe917d6b5984ae30
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2022-01-27 21:23:46 -0300

    refatoracao exercicio 1

--- exercises/Modulo-4-CS/bloco_32/dia_1/exercises/exercise1.py
+++ exercises/Modulo-4-CS/bloco_32/dia_1/exercises/exercise1.py
@@ -1,10 +1,9 @@
 # Exercício 1: Crie uma função que receba dois números e retorne o maior deles.
 
 def return_biggest_value(val1, val2):
-    if val1 > val2:
-        return f"O maior valor é: {val1}"
-    else:
-        return f"O maior valor é: {val2}"
+    b_val = f"O maior é: {val1}" if val1 > val2 else f"O maior é: {val2}"
+
+    return b_val
 
 
 print(return_biggest_value(10, 15))
